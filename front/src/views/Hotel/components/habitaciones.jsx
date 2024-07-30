ca06e00765d5457395eb2faf8534542fd846abf6
Cesar Narvaez <cesar.teacher24@gmail.com>
2024-07-30 09:43:39 -0500

    Implementa imagenes slider en las habitaciones del hotel

--- front/src/views/Hotel/components/habitaciones.jsx
+++ front/src/views/Hotel/components/habitaciones.jsx
@@ -1,65 +1,15 @@
 import React from "react";
+import { habitaciones } from "../data/habitaciones";
+import { ImagenSlider } from "../../../components/ImagenSlider";
 
 export const Habitaciones = () => {
-  const habitaciones = [
-    {
-      nombre: "Tatacoa",
-      imagen: "https://clubcampestreneiva.site/IMG/Hotel/720/hotel_club(3).jpg",
-    },
-    {
-      nombre: "San Agustin",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(5).jpg",
-    },
-    {
-      nombre: "Yuma",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(7).jpg",
-    },
-    {
-      nombre: "Bordones",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(4).jpg",
-    },
-    {
-      nombre: "Guacharos",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(4).jpg",
-    },
-    {
-      nombre: "Oasis",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(3).jpg",
-    },
-    {
-      nombre: "San Pedro",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(6).jpg",
-    },
-    {
-      nombre: "Cedral",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(1).jpg",
-    },
-    {
-      nombre: "Guadalupe",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(7).jpg",
-    },
-    {
-      nombre: "Guatipan",
-      imagen:
-        "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(2).jpg",
-    },
-  ];
-
   return (
     <>
-      <div className=' w-full flex flex-col gap-4 justify-center items-center py-10'>
-        <h2 className=' text-xl font-thin italic'>Nuestras Cabañas</h2>
-        <h2 className=' font-semibold text-xl'>Deluxe con 2 Camas grandes</h2>
-        <div className='border-t border-amber-300 w-[20rem] mt-3 '></div>
-        <div className=' w-[50rem] py-7 text-justify font-thin sm:max-md:w-full sm:max-md:px-5 '>
+      <div className=" w-full flex flex-col gap-4 justify-center items-center py-10">
+        <h2 className=" text-xl font-thin italic">Nuestras Cabañas</h2>
+        <h2 className=" font-semibold text-xl">Deluxe con 2 Camas grandes</h2>
+        <div className="border-t border-amber-300 w-[20rem] mt-3 "></div>
+        <div className=" w-[50rem] py-7 text-justify font-thin sm:max-md:w-full sm:max-md:px-5 ">
           <p>
             El hotel cuenta con 10 cómodas y confortables cabañas, las cuales
             han sido decoradas con bellos paisajes y sitios turísticos de
@@ -68,23 +18,18 @@ export const Habitaciones = () => {
             agradable ambiente, en un encuentro único con la naturaleza.
           </p>
         </div>
-        <div className=' grid lg:grid-cols-2 grid-cols-3 gap-24 py-8 sm:max-md:grid-cols-1 sm:max-md:gap-10'>
+        <div className=" grid lg:grid-cols-2 grid-cols-3 gap-24 py-8 sm:max-md:grid-cols-1 sm:max-md:gap-10">
           {habitaciones.map((habitacion, index) => (
             <div
-              className='relative animate__animated animate__fadeIn'
+              className="relative animate__animated animate__fadeIn"
               key={index}
             >
-              <img
-                src={habitacion.imagen}
-                alt={habitacion.nombre}
-                border='0'
-                className='w-[29rem] object-contain sm:max-md:w-full sm:max-md:object-cover sm:max-md:h-full sm:max-md:rounded-md sm:max-md:px-5'
-              />
-              <div className='absolute h-12 w-36 top-2/3 -right-16 border border-black bg-white rounded-md sm:max-md:left-10 sm:max-md:w-24'>
-                <div className=' h-full flex justify-center items-center font-light italic'>
+              <div className="absolute h-12 w-36 top-[80%] -right-5 border border-black bg-white rounded-md sm:max-md:left-10 sm:max-md:w-24 z-40">
+                <div className=" h-full flex justify-center items-center font-light italic">
                   <h3>{habitacion.nombre}</h3>
                 </div>
               </div>
+              <ImagenSlider imagenes={habitacion.imagenes} />
             </div>
           ))}
         </div>
---
+++ front/src/views/Hotel/data/habitaciones.js
@@ -0,0 +1,95 @@
+export const habitaciones = [
+  {
+    nombre: "BORDONES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(4).jpg",
+    ],
+  },
+  {
+    nombre: "CEDRALES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(4).jpeg",
+    ],
+  },
+  {
+    nombre: "GUACHAROS",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(2).jpeg",
+    ],
+  },
+  {
+    nombre: "GUADUALES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(4).jpeg",
+    ],
+  },
+  {
+    nombre: "GUAITIPÁN",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(3).jpeg",
+    ],
+  },
+  {
+    nombre: "OASIS",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(5).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(6).jpeg",
+    ],
+  },
+  {
+    nombre: "SAN AGUSTÍN",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(5).jpeg",
+    ],
+  },
+  {
+    nombre: "SAN PEDRO",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(2).jpeg",
+    ],
+  },
+  {
+    nombre: "TATACOA",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(5).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(6).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(7).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(8).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(9).jpeg",
+    ],
+  },
+  {
+    nombre: "YUMA",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(5).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(6).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(7).jpeg",
+    ],
+  },
+];
