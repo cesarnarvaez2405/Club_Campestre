d83b1b90f21376de059d34cfb677ded2ffac438b
Cesar Narvaez <cesar.teacher24@gmail.com>
2024-07-30 11:01:10 -0500

    Ajusta orden de las habitaciones

--- front/src/views/Hotel/data/habitaciones.js
+++ front/src/views/Hotel/data/habitaciones.js
@@ -1,72 +1,5 @@
 export const habitaciones = [
   {
-    nombre: "BORDONES",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(4).jpg",
-    ],
-  },
-  {
-    nombre: "CEDRALES",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(2).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(3).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(4).jpeg",
-    ],
-  },
-  {
-    nombre: "GUACHAROS",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(2).jpeg",
-    ],
-  },
-  {
-    nombre: "GUADUALES",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(2).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(3).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(4).jpeg",
-    ],
-  },
-  {
-    nombre: "GUAITIPÁN",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(2).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(3).jpeg",
-    ],
-  },
-  {
-    nombre: "OASIS",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(2).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(3).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(4).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(5).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(6).jpeg",
-    ],
-  },
-  {
-    nombre: "SAN AGUSTÍN",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(2).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(3).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(4).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(5).jpeg",
-    ],
-  },
-  {
-    nombre: "SAN PEDRO",
-    imagenes: [
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(1).jpeg",
-      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(2).jpeg",
-    ],
-  },
-  {
     nombre: "TATACOA",
     imagenes: [
       "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Tatacoa/tatacoa_hotel(1).jpeg",
@@ -81,6 +14,16 @@ export const habitaciones = [
     ],
   },
   {
+    nombre: "SAN AGUSTÍN",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_agustin/san_agustin_hotel(5).jpeg",
+    ],
+  },
+  {
     nombre: "YUMA",
     imagenes: [
       "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(1).jpeg",
@@ -90,6 +33,63 @@ export const habitaciones = [
       "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(5).jpeg",
       "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(6).jpeg",
       "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Yuma/yuma_hotel(7).jpeg",
+    ],
+  },
+  {
+    nombre: "BORDONES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Hotel_club(4).jpg",
+    ],
+  },
+  {
+    nombre: "GUACHAROS",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guacharos/guacharos_hotel(2).jpeg",
+    ],
+  },
+  {
+    nombre: "OASIS",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(4).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(5).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Oasis/oasis_hotel(6).jpeg",
+    ],
+  },
+  {
+    nombre: "GUAITIPÁN",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaitipan/guaitipan_hotel(3).jpeg",
+    ],
+  },
+  {
+    nombre: "GUADUALES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Guaduales/guaduales_hotel(4).jpeg",
+    ],
+  },
+  {
+    nombre: "SAN PEDRO",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/San_pedro/san_pedro_hotel(2).jpeg",
+    ],
+  },
+  {
+    nombre: "CEDRALES",
+    imagenes: [
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(1).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(2).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(3).jpeg",
+      "https://clubcampestreneiva.site/IMG/Hotel/Habitaciones/Cedrales/cedrales_hotel(4).jpeg",
     ],
   },
 ];
